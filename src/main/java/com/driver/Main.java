836be56b2b78d0b730a4ae477a0d83458dcc5cea
S Abida <abidashaik.c@gmail.com>
2022-10-09 14:26:10 +0530

    Assignment Update 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+//        obj.name="Abida";
+//        System.out.print(obj.name);
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("Abida");
+        System.out.println(obj.getName());
+
+
+    }
 }
